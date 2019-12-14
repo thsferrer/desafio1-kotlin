8f38c88a918bbe56dde47bdee51421528acb2302
Thais <34093866+thsferrer@users.noreply.github.com>
2019-12-14 15:39:40 -0300

    Update Login.kt

--- app/src/main/java/com/example/trabalho1/Login.kt
+++ app/src/main/java/com/example/trabalho1/Login.kt
@@ -1,2 +1,13 @@
 package com.example.trabalho1
 
+import java.io.Serializable
+
+data class Login (
+  var username: String,
+  var password: String
+) : Serializable{
+  
+  override fun toString(): String{
+    return this.username
+  }
+}
